9d5d69c866a0fa45884b4efbb50f2f148bbea0eb
edisond <edisond@qq.com>
2014-03-26 14:13:41 +0800

    test

--- Default.aspx
+++ Default.aspx
@@ -1,159 +1,49 @@
-﻿<%@ page language="C#" masterpagefile="~/Site.master" autoeventwireup="true" inherits="_Default, App_Web_unt4kimq" %>
+﻿<%@ page language="C#" autoeventwireup="true" inherits="_Default, App_Web_fp1fwhwj" %>
 
-
-<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-<div class="mainbody">
-  <div class="info">
-    <div style="border-radius: 6px;float:left;overflow: hidden;display:inline;">
-    
-    <div class="container">
-	<section class="cr-container">				
-		<input id="select-img-1" name="radio-set-1" type="radio" class="cr-selector-img-1" checked="checked"/>
-		<label for="select-img-1" class="cr-label-img-1"></label>
-		<input id="select-img-2" name="radio-set-1" type="radio" class="cr-selector-img-2" />
-		<label for="select-img-2" class="cr-label-img-2"></label>
-		<input id="select-img-3" name="radio-set-1" type="radio" class="cr-selector-img-3" />
-		<label for="select-img-3" class="cr-label-img-3"></label>
-		<input id="select-img-4" name="radio-set-1" type="radio" class="cr-selector-img-4" />
-		<label for="select-img-4" class="cr-label-img-4"></label>
-		<div class="cr-bgimg">
-			<div><span></span><span></span><span></span><span></span></div>
-            <div><span></span><span></span><span></span><span></span></div>
-            <div><span></span><span></span><span></span><span></span></div>
-            <div><span></span><span></span><span></span><span></span></div>
-	    </div>
-	</section>
-    </div>
- 
-
-    </div>
-    <div class="card">
-      <h1>名片</h1>
-      <p>blabla</p>
-      <p>blabla</p>
-      <p>blabla</p>
-      <p>blabla</p>
-      <ul class="linkmore">
-        <li><a href="Default.aspx" class="talk" title="留言"></a></li>
-        <li><a href="Default.aspx" class="address" title="地址"></a></li>
-        <li><a href="Default.aspx" class="email" title="写信"></a></li>
-        <li><a href="Default.aspx" class="more" title="更多"></a></li>
-        <li><a href="Default.aspx" class="heart" title="关注"></a></li>
-      </ul>
-    </div>
-    <div class="search">
-        <asp:TextBox class="searchTextbox" runat="server" ID="search"/>
-        <asp:Button class="searchButton" ID="Button1" runat="server" text="Go"/>
-        </div>
-    </div>
-  
-  <div style="width: 1000px;margin: 0 auto;" >
-    <ul style="width: 666px; float: left;" >
-      <li style="padding-bottom:20px">
-        <div class="blogbox">
-          
-          <h2 class="title"><a href="Default.aspx">blabla</a></h2>
-          
-           
-            <p>QAZWSXEDCRFVTGBYHNUJMIKOLPqazwsxedcrfvtgbyhnujmikolp</p>
-          
-          <ul class="blogbox_details">
-            <li class="blogbox_likes">10</li>
-            <li class="blogbox_comments">34</li>
-            <li class="blogbox_time">2013-8-7</li>
-          </ul>
-        </div>
-        <!--arrow_box end--> 
-      </li>
-      <li style="padding-bottom:20px">
-        <div class="blogbox">
-          <h2 class="title"><a href="Default.aspx">blabla</a></h2>
-          <ul class="textinfo">
-            <p>blabla</p>
-          </ul>
-          <ul class="blogbox_details">
-            <li class="blogbox_likes">102</li>
-            <li class="blogbox_comments">58</li>
-            <li class="blogbox_time">2013-8-7</li>
-          </ul>
-        </div>
-        <!--arrow_box end-->  
-      </li>
-      <li style="padding-bottom:20px">
-        <div class="blogbox">
-          <h2 class="title"><a href="Default.aspx">blabla</a></h2>
-          <ul class="textinfo">
-            <p>blabla</p>
-          </ul>
-          <ul class="blogbox_details">
-            <li class="blogbox_likes">102</li>
-            <li class="blogbox_comments">58</li>
-            <li class="blogbox_time">2013-8-7</li>
-          </ul>
-        </div>
-         
-      </li>
-    </ul>
-    
-    <aside>
-        
-        <div class="notice">
-        <h2>公告</h2>
-        <p>正在完善主页。</p>
-        <time>2012-3-21</time>
-        </div>
-      <div class="click">
-        <h2>点击</h2>
-        <ol>
-          <li><span>0<strong>1</strong></span><a href="Default.aspx">blabla</a></li>
-          <li><span>0<strong>2</strong></span><a href="Default.aspx">blabla</a></li>
-          <li><span>0<strong>3</strong></span><a href="Default.aspx">blabla</a></li>
-          <li><span>0<strong>4</strong></span><a href="Default.aspx">blabla</a></li>
-          <li><span>0<strong>5</strong></span><a href="Default.aspx">blabla</a></li>
-          <li><span>0<strong>6</strong></span><a href="Default.aspx">blabla</a></li>
-          <li><span>0<strong>7</strong></span><a href="Default.aspx">blabla</a></li>
-          <li><span>0<strong>8</strong></span><a href="Default.aspx">blabla</a></li>
-          <li><span>0<strong>9</strong></span><a href="Default.aspx">blabla</a></li>
-        </ol>
-      </div>
-      <div class="visitors">
-      <h2>动态</h2>
-      <dl>
-       
-        <dd>blabla
-          
-        </dd>
-        <dd>在 <a href="Default.aspx" class="title">blabla</a>中评论：</dd>
-        <dd style="color:#00AEAE">blabla</dd>
-        <time>49分钟前</time>
-      </dl>
-      <dl>
-        
-        <dd>blabla
-          
-        </dd>
-        <dd>在 <a href="Default.aspx" class="title">blabla</a>中评论：</dd>
-        <dd style="color:#00AEAE">blabla</dd>
-        <time>2小时前</time>
-      </dl>
-      <dl>
-        
-        <dd>blabla
-          
-        </dd>
-        <dd>在 <a href="Default.aspx" class="title">blabla</a>中评论：</dd>
-        <dd style="color:#00AEAE">blabla</dd>
-        <time>8月7日</time>
-      </dl>
-    </div> 
-      
-      
-      
-    </aside>
-  </div>
-  <!--blogs end--> 
-</div>
-<!--mainbody end-->
-
-
-</asp:Content>
+<!DOCTYPE html />
+<html>
+<head>
+    <link type="text/css" rel="stylesheet" href="posters.css">
+	<script type="text/javascript" src="jquery/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="jquery/background.js"></script>
+    <script type="text/javascript" src="jquery/posters.js"></script>
+  <style>
+    body {
+      background: Black;
+    }
+    canvas {
+      display: block;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      position:absolute;
+    }
+	</style>
+	<script>
+	    $(document).ready(function () {
+	        $("canvas.snow").let_it_snow();
+            var posters1 = new posters('posters1', { className: "posters" },
+        [{ "img": "images\/1.jpg", "url": "images\/1.jpg" },
+        { "img": "images\/2.jpg", "url": "images\/2.jpg" },
+        { "img": "images\/3.jpg", "url": "images\/3.jpg" },
+        { "img": "images\/4.jpg", "url": "images\/4.jpg" },
+        { "img": "images\/5.jpg", "url": "images\/5.jpg" },
+        { "img": "images\/6.jpg", "url": "images\/6.jpg" },
+        { "img": "images\/7.jpg", "url": "images\/7.jpg" },
+        { "img": "images\/8.jpg", "url": "images\/8.jpg" },
+        { "img": "images\/9.jpg", "url": "images\/9.jpg" },
+        { "img": "images\/10.jpg", "url": "images\/10.jpg" },
+        { "img": "images\/11.jpg", "url": "images\/11.jpg" },
+        { "img": "images\/12.jpg", "url": "images\/12.jpg" },
+        { "img": "images\/13.jpg", "url": "images\/13.jpg" },
+        { "img": "images\/14.jpg", "url": "images\/14.jpg"}]
+        );
+        });
+	</script>
+</head>
+<body>
+  <canvas width="100%" height="100%" class="snow"></canvas>
+  <center><div id="posters1"></div></center>
+</body>
+</html>
---
+++ jquery/posters.js
@@ -0,0 +1,202 @@
+(function (jq) {
+    var posters = function (o, options, data) {
+        this.parent = jq("#" + o);
+        if (this.parent.length <= 0) {
+            return false;
+        }
+        this.options = jq.extend({}, posters.options, options);
+        if (typeof (data) !== 'object') return false;
+        this.data = data || {};
+        this.reset();
+    };
+    posters.options = {
+        direct: "left", //滚动的方向
+        initPage: 1, //默认当前显示第几条
+        className: "posterTvGrid", //最外层样式
+        width: 0, //最外层宽
+        height: 0, //最外层高  
+        delay: 1000000, //滚动间隔（毫秒）
+        speed: 500 //滚动速度毫秒
+    };
+    posters.prototype = {
+        reset: function (options) {
+            if (typeof (options) == 'object') {
+                jq.extend(this.options, options);
+            }
+            this.indentation = 50;//缩进宽
+            this.opacity = 0.6;//不透明度
+            this.total = this.data.length; //页数
+            this.pageNow = this.options.initPage; //当前页
+            this.pageNowWidth = 250;//当前页宽
+            this.pageNowHeight = 375;//当前页高
+            this.page2Width = this.pageNowWidth*0.8;//页2宽
+            this.page2Height = this.pageNowHeight*0.8;//页2高
+            this.page3Width = this.page2Width*0.8;//页3宽
+            this.page3Height = this.page2Height*0.8;//页3高
+            this.options.width = this.pageNowWidth+this.page2Width*2+this.page3Width*2-this.indentation*4; //总宽
+            this.options.height=this.pageNowHeight;
+            this.pageNowLeft = this.page3Width+this.page2Width-this.indentation*2;//当前页左边界
+            this.preLeft = this.page3Width-this.indentation;; //上一页左边界
+            this.nextLeft = this.pageNowLeft+this.pageNowWidth-this.indentation; //下一页左边界
+            this.pre2Left = 0; //上两页左边界
+            this.next2Left = this.nextLeft+this.page2Width-this.indentation; //下两页左边界
+            this.disappearLeft=0-this.page3Width;//左消失页左边界
+            this.disappearRight=this.options.width;//右消失页左边界
+            this.drawContent();
+        },
+        drawContent: function () {
+            this.parent.empty();
+            this.parent.css({ width: this.options.width + "px", height: this.options.height + "px", position: "relative" });
+            this.content = document.createElement("DIV");
+            this.content.className = this.options.className;
+            this.content.cssText = "width:" + this.options.width + "px;height:" + this.options.height + "px;cursor:move;position:absolute;";
+            this.contentHolder = document.createElement("DIV");
+            this.contentHolder.style.width = this.options.width + 'px';
+            this.contentHolder.style.height = this.options.height + 'px';
+            for (var item = 0, i = 1, l = this.data.length; item < l; ++item, ++i) {
+                var contentHolderUnit = document.createElement("DIV");
+                contentHolderUnit.className = "contentHolderUnit";
+                contentHolderUnit.setAttribute("ref", i);
+                contentHolderUnit.setAttribute("id", 'contentHolderUnit' + (i));
+                var unitItem = '<a href="' + this.data[item].url + '" target="_blank">';
+                unitItem += '<img src="' + this.data[item].img + '" alt="' + this.data[item].title + '"/>';
+                unitItem += '</a>';
+                contentHolderUnit.innerHTML = unitItem;
+                this.contentHolder.appendChild(contentHolderUnit);
+            }
+            this.content.appendChild(this.contentHolder);
+            this.parent.append(this.content);
+            this.parent.css({ overflow: 'hidden' });
+            this.initContent();
+            this.bind();
+            this.start();
+        },
+        initContent: function () {
+            contentHolderUnit = this.parent.find(".contentHolderUnit");
+            contentHolderUnit.css({ width: '0px', height: '0px', opacity: 0, left: this.options.width / 2 + 'px', zIndex: 0 });
+            this.parent.find(".contentHolderUnit:nth-child(" + this.pageNow + ")").css({ width: this.pageNowWidth+"px", height: this.pageNowHeight+"px", opacity: 1, left: this.pageNowLeft + 'px', zIndex: 3, marginTop: 0 });
+            var pre = this.pageNow > 1 ? this.pageNow - 1 : this.total;
+            var next = this.pageNow == this.total ? 1 : this.pageNow + 1;
+            var pre2 = pre > 1 ? pre-1 : this.total;
+            var next2 = next == this.total ? 1 : next + 1;
+            this.parent.find(".contentHolderUnit:nth-child(" + pre + ")").css({ opacity: this.opacity, left: this.preLeft + 'px', height: this.page2Height+"px", width: this.page2Width+"px", zIndex: 2, marginTop: '75px' });
+            this.parent.find(".contentHolderUnit:nth-child(" + next + ")").css({ opacity: this.opacity, left: this.nextLeft + 'px', height: this.page2Height+"px", width: this.page2Width+"px", zIndex: 2, marginTop: '75px' });
+            this.parent.find(".contentHolderUnit:nth-child(" + pre2 + ")").css({ opacity: this.opacity, left: this.pre2Left + 'px', height: this.page3Height+"px", width: this.page3Width+"px", zIndex: 1, marginTop: '150px' });
+            this.parent.find(".contentHolderUnit:nth-child(" + next2 + ")").css({ opacity: this.opacity, left: this.next2Left + 'px', height: this.page3Height+"px", width: this.page3Width+"px", zIndex: 1, marginTop: '150px' });
+        },
+        bind: function () {
+            this.lists = this.parent.find(".contentHolderUnit");
+            var _this = this;
+            this.parent.mouseover(function () {
+                _this.stop();
+            });
+            this.parent.mouseout(function () {
+                _this.start();
+            });
+            _this.lists.click(function (e) {
+                var ref = parseInt(this.getAttribute("ref"));
+                if (_this.pageNow == ref) {
+                    return true;
+                } else {
+                    e.preventDefault();
+                }
+                if (_this.pageNow < ref) {
+                    var rightAbs = ref - _this.pageNow;
+                    var leftAbs = _this.pageNow + _this.total - ref;
+                } else {
+                    var rightAbs = _this.total - _this.pageNow + ref;
+                    var leftAbs = _this.pageNow - ref;
+                }
+                if (leftAbs < rightAbs) {
+                    dir = "right";
+                } else {
+                    dir = "left";
+                }
+                _this.turnpage(ref, dir);
+
+            });
+        },
+        start: function () {
+            var _this = this;
+            if (_this.timerId) _this.stop();
+            _this.timerId = setInterval(function () {
+                if (_this.options.direct == "left") {
+                    _this.turn("left");
+                } else {
+                    _this.turn("right");
+                }
+            }, _this.options.delay);
+        },
+        stop: function () {
+            clearInterval(this.timerId);
+        },
+        turn: function(dir){
+			var _this = this;
+			
+			if(dir == "right"){
+				var page = _this.pageNow -1;
+				if(page <= 0) page = _this.total;
+			}else{
+				var page = _this.pageNow + 1;
+				if(page > _this.total) page = 1;
+			}
+			_this.turnpage(page, dir);
+		},
+        turnpage: function (page, dir) {
+            var _this = this;
+            if (_this.locked) return false;
+            _this.locked = true;
+            if (_this.pageNow == page) return false;
+            var run = function (page, dir, t) {
+                var pre = page > 1 ? page - 1 : _this.total;
+                var next = page == _this.total ? 1 : page + 1;
+                var pre2 = pre - 1 >= 1 ? pre - 1 : _this.total;
+                var next2 = next + 1 > _this.total ? 1 : next + 1;
+                var pre3 = pre2 - 1 >= 1 ? pre2 - 1 : _this.total;
+                var next3 = next2 + 1 > _this.total ? 1 : next2 + 1;
+                var pre4 = pre3 - 1 >= 1 ? pre3 - 1 : _this.total;
+                var next4 = next3 + 1 > _this.total ? 1 : next3 + 1;
+               
+                
+                if (dir == 'left') {
+                    
+                    _this.parent.find(".contentHolderUnit:nth-child(" + page + ")").css({zIndex:4});
+                    if (page-_this.pageNow==2){
+                        _this.parent.find(".contentHolderUnit:nth-child(" + pre3 + ")").css({zIndex:1});
+                        _this.parent.find(".contentHolderUnit:nth-child(" + pre2 + ")").css({zIndex:2});
+                        _this.parent.find(".contentHolderUnit:nth-child(" + pre + ")").css({zIndex:3});
+                    }
+                    _this.parent.find(".contentHolderUnit:nth-child(" + pre3 + ")").animate({ width: _this.page3Width+"px", height: _this.Page3Height+"px", opacity: 0, left: _this.disappearLeft + 'px', zIndex: 1, marginTop: '150px' }, t);
+                    _this.parent.find(".contentHolderUnit:nth-child(" + pre4 + ")").animate({ width: _this.page3Width+"px", height: _this.Page3Height+"px", opacity: 0, left: _this.disappearLeft + 'px', zIndex: 0, marginTop: '150px' }, t);
+                    _this.parent.find(".contentHolderUnit:nth-child(" + page + ")").animate({ opacity: 1, left: _this.pageNowLeft + 'px', height: _this.pageNowHeight+"px", width: _this.pageNowWidth+"px", zIndex: 4, marginTop: '0' }, t);
+                    _this.parent.find(".contentHolderUnit:nth-child(" + pre + ")").animate({ opacity: _this.opacity, left: _this.preLeft + "px", height:_this.page2Height+"px", width: _this.page2Width+"px", zIndex: 3, marginTop: '75px' }, t);
+                    _this.parent.find(".contentHolderUnit:nth-child(" + next + ")").animate({ opacity: _this.opacity, left: _this.nextLeft + "px", height:_this.page2Height+"px", width: _this.page2Width+"px", zIndex: 3, marginTop: '75px' }, t);
+                    _this.parent.find(".contentHolderUnit:nth-child(" + pre2 + ")").animate({ width: _this.page3Width+"px", height: _this.page3Height+"px", opacity: _this.opacity, left: _this.pre2Left + 'px', zIndex: 2, marginTop: '150px' }, t);
+                    _this.parent.find(".contentHolderUnit:nth-child(" + next2 + ")").animate({ width: _this.page3Width+"px", height:_this.page3Height+"px", opacity: _this.opacity, left: _this.next2Left + 'px', zIndex: 2, marginTop: '150px' }, t, "", function () { _this.locked = false; });
+                    
+                } else {
+                    _this.parent.find(".contentHolderUnit:nth-child(" + page + ")").css({zIndex:4});
+                    if (page-_this.pageNow==-2){
+                        _this.parent.find(".contentHolderUnit:nth-child(" + next3 + ")").css({zIndex:1});
+                        _this.parent.find(".contentHolderUnit:nth-child(" + next2 + ")").css({zIndex:2});
+                        _this.parent.find(".contentHolderUnit:nth-child(" + next + ")").css({zIndex:3});
+                    }
+                    _this.parent.find(".contentHolderUnit:nth-child(" + next3 + ")").animate({ width: _this.page3Width+"px", height: _this.Page3Height+"px", opacity: 0, left: _this.disappearRight + 'px', zIndex: 1, marginTop: '150px' }, t);
+                    _this.parent.find(".contentHolderUnit:nth-child(" + next4 + ")").animate({ width: _this.page3Width+"px", height: _this.Page3Height+"px", opacity: 0, left: _this.disappearRight + 'px', zIndex: 0, marginTop: '150px' }, t);
+                    _this.parent.find(".contentHolderUnit:nth-child(" + page + ")").animate({ opacity: 1, left: _this.pageNowLeft + 'px', height: _this.pageNowHeight+"px", width: _this.pageNowWidth+"px", zIndex: 4, marginTop: '0' }, t);
+                    _this.parent.find(".contentHolderUnit:nth-child(" + next + ")").animate({ opacity: _this.opacity, left: _this.nextLeft + "px", height:_this.page2Height+"px", width: _this.page2Width+"px", zIndex: 3, marginTop: '75px' }, t);
+                    _this.parent.find(".contentHolderUnit:nth-child(" + pre + ")").animate({ opacity: _this.opacity, left: _this.preLeft + "px", height:_this.page2Height+"px", width: _this.page2Width+"px", zIndex: 3, marginTop: '75px' }, t);
+                    _this.parent.find(".contentHolderUnit:nth-child(" + next2 + ")").animate({ width: _this.page3Width+"px", height: _this.page3Height+"px", opacity: _this.opacity, left: _this.next2Left + 'px', zIndex: 2, marginTop: '150px' }, t);
+                    _this.parent.find(".contentHolderUnit:nth-child(" + pre2 + ")").animate({ width: _this.page3Width+"px", height:_this.page3Height+"px", opacity: _this.opacity, left: _this.pre2Left + 'px', zIndex: 2, marginTop: '150px' }, t, "", function () { _this.locked = false; });
+                    
+                }
+
+                _this.pageNow = page;
+
+            };
+            run(page, dir, _this.options.speed);
+        }
+
+    };
+    window.posters = posters;
+})(jQuery);
